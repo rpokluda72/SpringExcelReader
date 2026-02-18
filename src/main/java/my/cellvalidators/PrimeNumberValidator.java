c54dd9df5c44c8208338cb252a78ea47d8bd2e99
rpokluda72 <roman.pokluda@gmail.com>
2026-02-18 09:11:56 +0100

    improvement

--- src/main/java/my/cellvalidators/PrimeNumberValidator.java
+++ src/main/java/my/cellvalidators/PrimeNumberValidator.java
@@ -2,12 +2,16 @@ package my.cellvalidators;
 
 import my.utils.NumericUtils;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 
 public class PrimeNumberValidator implements CellValidator {
 
     @Override
     public boolean isValid(Cell cell) {
         try {
+            if (!cell.getCellType().equals(CellType.NUMERIC)) {
+                return false;
+            }
             double cellValue = cell.getNumericCellValue();
 
             return NumericUtils.isInteger(cellValue) && NumericUtils.isPrime((int) cellValue);
